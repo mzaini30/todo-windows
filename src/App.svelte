8da54005267d40b509f5df2e4de778e58142303d
mzaini30 <muhzaini30@gmail.com>
2024-01-18 08:27:40 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,23 @@
 <script>
-  import { Card } from "flowbite-svelte";
+  import { Card, Toggle } from "flowbite-svelte";
+  let data = [];
+  // data: id, todo, isDone
+  if (localStorage.getItem("data")) {
+    data = JSON.parse(localStorage.getItem("data"));
+  }
 </script>
 
 <svelte:head>
   <title>TODO</title>
 </svelte:head>
 
-<Card>
+<Card class="max-w-full">
   <h5
-    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
   >
     TODO
   </h5>
-  <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
-    Here are the biggest enterprise technology acquisitions of 2021 so far, in
-    reverse chronological order.
-  </p>
+  <div class="mb-3">
+    <Toggle checked={true}>Checked toggle</Toggle>
+  </div>
 </Card>
