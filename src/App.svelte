84ba1928f1a16d674d6a71a71315fba0dbc255fb
mzaini30 <muhzaini30@gmail.com>
2024-01-23 22:37:11 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,7 @@
     A,
   } from "flowbite-svelte";
   import { Orm } from "./localstorage-orm";
+  import Gerbang from "./kunci/src/Gerbang.svelte";
 
   let tambahBaru = "";
   let database = { todo: [] };
@@ -27,62 +28,64 @@
   <title>TODO</title>
 </svelte:head>
 
-<form
-  on:submit|preventDefault={() => {
-    database = new Orm(database)
-      .insert("todo", { todonya: tambahBaru, isDone: false })
-      .done();
-    localStorage.database = JSON.stringify(database);
-    tambahBaru = "";
-  }}
->
-  <div class="p-4 pb-0">
-    <div>
-      <Label for="first_name" class="mb-2">TODO Baru</Label>
-      <Input
-        type="text"
-        id="first_name"
-        bind:value={tambahBaru}
-        placeholder="Ketik di sini"
-        required
-      />
+<Gerbang namaAplikasi="TODO">
+  <form
+    on:submit|preventDefault={() => {
+      database = new Orm(database)
+        .insert("todo", { todonya: tambahBaru, isDone: false })
+        .done();
+      localStorage.database = JSON.stringify(database);
+      tambahBaru = "";
+    }}
+  >
+    <div class="p-4 pb-0">
+      <div>
+        <Label for="first_name" class="mb-2">TODO Baru</Label>
+        <Input
+          type="text"
+          id="first_name"
+          bind:value={tambahBaru}
+          placeholder="Ketik di sini"
+          required
+        />
+      </div>
+    </div>
+  </form>
+
+  <div class="p-4">
+    <div class="grid grid-cols-2 gap-4">
+      <Card class="max-w-full ">
+        <h5
+          class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          Belum Selesai
+        </h5>
+        {#each database.todo as item}
+          {#if item.isDone == false}
+            <div class="mb-3">
+              <Toggle checked={false} on:click={() => ubah(item.id, true)}
+                >{item.todonya}</Toggle
+              >
+            </div>
+          {/if}
+        {/each}
+      </Card>
+      <Card class="max-w-full ">
+        <h5
+          class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          Sudah Selesai
+        </h5>
+        {#each database.todo as item}
+          {#if item.isDone == true}
+            <div class="mb-3">
+              <Toggle checked={true} on:click={() => ubah(item.id, false)}
+                >{item.todonya}</Toggle
+              >
+            </div>
+          {/if}
+        {/each}
+      </Card>
     </div>
   </div>
-</form>
-
-<div class="p-4">
-  <div class="grid grid-cols-2 gap-4">
-    <Card class="max-w-full ">
-      <h5
-        class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-      >
-        Belum Selesai
-      </h5>
-      {#each database.todo as item}
-        {#if item.isDone == false}
-          <div class="mb-3">
-            <Toggle checked={false} on:click={() => ubah(item.id, true)}
-              >{item.todonya}</Toggle
-            >
-          </div>
-        {/if}
-      {/each}
-    </Card>
-    <Card class="max-w-full ">
-      <h5
-        class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-      >
-        Sudah Selesai
-      </h5>
-      {#each database.todo as item}
-        {#if item.isDone == true}
-          <div class="mb-3">
-            <Toggle checked={true} on:click={() => ubah(item.id, false)}
-              >{item.todonya}</Toggle
-            >
-          </div>
-        {/if}
-      {/each}
-    </Card>
-  </div>
-</div>
+</Gerbang>
